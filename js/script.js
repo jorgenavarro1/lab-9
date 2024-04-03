9cad8e9fc6483696035e1cf73652330ae865a5a5
jorgenavarro1 <jnavarro11@hawk.iit.edu>
2024-04-02 22:24:36 -0500

    Creating JavaScript

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function init(){
+  var button = document.getElementById('entrybutton');
+  
+  function showMeText(){
+    var text = document.getElementById('entryinput');
+    document.getElementById('textoutput').innerHTML = text.value;
+    alert("Jorge Navarro: " + text.value);
+  }
 
- 
-
-
-
-
-
-
-
+  button.addEventListener('click', showMeText);
+  }
+  
 window.addEventListener('load', init);
